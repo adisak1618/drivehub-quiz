a8f5a10d9d67713c8f99b745051615fded37656e
adisak-divemotion <adisak@divemotion.com>
2022-10-05 00:19:22 +0700

    number input component

--- packages/ui/Input/numberInput.tsx
+++ packages/ui/Input/numberInput.tsx
@@ -22,32 +22,26 @@ export const NumberInput = ({
   };
 
   return (
-    <div className="flex w-full gap-6">
+    <div className="flex w-full gap-1">
       <div>
         <div
           onClick={() => {
             Number(value) > 0 && onChange((Number(value) - 1).toString());
             onDecrease?.((Number(value) - 1).toString());
           }}
-          className="flex h-[44px] w-[44px] cursor-pointer items-center justify-center rounded-full border border-gray-4 bg-white hover:border-gray-5 hover:shadow-sm"
+          className="flex h-[28px] w-[28px] cursor-pointer items-center justify-center rounded-md border border-gray-4 bg-white hover:border-gray-5 hover:shadow-sm"
         >
           -
         </div>
       </div>
-      <input
-        type="number"
-        className="h-[44px] w-full rounded-full border border-gray-4 px-4 text-center"
-        onChange={numberOnChange}
-        value={value}
-        min="0"
-      />
+      <div className="flex text-center items-center px-2">{value}</div>
       <div>
         <div
           onClick={() => {
             onChange((Number(value) + 1).toString());
             onIncrease?.((Number(value) + 1).toString());
           }}
-          className="flex h-[44px] w-[44px] cursor-pointer items-center justify-center rounded-full border border-gray-4 bg-white hover:border-gray-5 hover:shadow-sm"
+          className="flex h-[28px] w-[28px] cursor-pointer items-center justify-center rounded-md border border-gray-4 bg-white hover:border-gray-5 hover:shadow-sm"
         >
           +
         </div>
