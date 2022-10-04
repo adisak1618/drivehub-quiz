e6992ae5043cdaf60169b1eae9af67a276c7b449
adisak-divemotion <adisak@divemotion.com>
2022-10-05 00:20:20 +0700

    remove old button component

--- packages/ui/Button.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-import tw from "twin.macro";
-
-export const Button = styled.div`
-  ${tw`bg-gray-3 text-center px-4 py-2 rounded-md hover:bg-gray-4 cursor-pointer`}
-`;
