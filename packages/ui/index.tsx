6952bb16f6c9d0989ac05acf8c5a29cc8251da74
adisak-divemotion <adisak@divemotion.com>
2022-10-05 00:19:47 +0700

    export new component

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,6 +1,9 @@
 export * from "./BgImg";
 export * from "./Button";
 export * from "./Card";
+export * from "./FormikInput";
+export * from "./Input";
+export * from "formik";
 export { default as React } from "react";
 export { default as ReactDom } from "react-dom";
 export { default as styled } from "styled-components";
