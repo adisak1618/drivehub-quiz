c5ec24131b56d0fe398593723990042169cf4a4d
adisak-divemotion <adisak@divemotion.com>
2022-10-05 02:14:03 +0700

    add input component

---
+++ packages/ui/FormikInput/input.tsx
@@ -0,0 +1,30 @@
+import { InputHTMLAttributes } from "react";
+import { useField } from "ui";
+
+import { Input } from "../Input/input";
+
+interface FormikNumberInputProps {
+  name: string;
+}
+
+export const FormikInput = (
+  props: FormikNumberInputProps & InputHTMLAttributes<HTMLInputElement>
+) => {
+  const { name, ...otherProps } = props;
+  const [{ value }, meta, helper] = useField<string>(name);
+  return (
+    <>
+      <Input
+        {...otherProps}
+        value={value}
+        onChange={(e) => {
+          helper.setTouched(true);
+          helper.setValue(e.target.value);
+        }}
+      />
+      {meta.error && meta.touched && (
+        <div className="mt-0.5 flex flex-wrap text-red-5">{meta.error}</div>
+      )}
+    </>
+  );
+};
